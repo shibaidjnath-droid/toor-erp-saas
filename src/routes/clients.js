72bba1ef7189cef112285da01fb1cb3f02605b09
shibaidjnath <shibaidjnath@gmail.com>
2025-10-19 20:51:19 +0200

    removed export contracts from client.js

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -2,7 +2,6 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
 import { pool } from "../db.js";
-import { contracts } from "./contracts.js";
 
 const router = express.Router();
 
